e6e7679a0c7bd4f47688b7ae0318f6d4266f318e
Zvyozdo4ka <74532816+Zvyozdo4ka@users.noreply.github.com>
2020-11-19 22:48:15 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,110 +5,103 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
  
     public static void main(String[] args) {
-        double num1 = getInt();
-        double num2 = getInt();
-        char operation = getOperation();
-        double result = calc(num1,num2,operation);
-        System.out.println("Output:\n"+result);
-    }
- 
-    public static double getInt(){
         System.out.println("Input:\n");
-         
-        double num;
-        
         String s;
         s = scanner.nextLine();
         //System.out.println(s);
-        
-        switch (s){
+        double result = calc(s);
+        if (result==22){
+            System.out.println("Ошибка");
+        }
+        else {
+        System.out.println("Output:\n"+result);
+        }
+    }
+    ////////////////////////////////////////////////////////    
+    public static double CheckDigit(String array_i){
+        double num1;
+        switch (array_i){
             case "1":
             case "I":
-                num=1;
+                num1=1;
                 break;
             case "2":
             case "II":
-                num=2;
+                num1=2;
                 break;
             case "3":
             case "III":
-                num=3;
+                num1=3;
                 break;
             case "4":
             case "IV":
-                num=4;
+                num1=4;
                 break;
 			case "5":
 			case "V":
-			    num=5;
+			    num1=5;
 				break;
 			case "6":
 			case "VI":
-				num=6;
+				num1=6;
 				break;
 			case "7":
 			case "VII":
-				num=7;
+				num1=7;
 				break;
 			case "8":
 			case "VIII":
-				num=8;
+				num1=8;
 				break;
 			case "9":
 			case "IX":
-				num=9;
+				num1=9;
 				break;
 			case "10":
 			case "X":
-			    num=10;
+			    num1=10;
 			default:
-                System.out.println("Цифра не распознана. Повторите ввод. По тз цифры из интервала [1;10].");
-                num = getInt();//рекурсия
+                System.out.println("Цифра не распознана. По тз цифры из интервала [1;10].");
+                num1=CheckDigit(array_i);
         }
-		
-        /*if(scanner.hasNextInt()){
-            num = scanner.nextInt();
-              
-        } else {
-            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            num = getInt();
-        }*/
-        return num;
+        return num1;
     }
-
-    public static char getOperation(){
-        System.out.println("Введите операцию:");
-        char operation;
-        if(scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            operation = getOperation();
-        }
-        return operation;
-    }
- 
-    public static double calc(double num1, double num2, char operation){
+    
+    public static double  calc(String s){
+        
+        double num1;
+        double num2;
         double result;
-        switch (operation){
-            case '+':
+        
+        String values[] = s.replaceAll("\\s", "").split("[\\+\\-\\*\\/]", 2);
+        String array_i1= values[0];
+        String array_i2= values[1];
+        num1=CheckDigit(array_i1);
+        num2=CheckDigit(array_i2);
+        //    взять операцию
+        String operand = s.replaceAll("[\\w\\s]", "");
+    
+        switch (operand){
+            case "+":
                 result = num1+num2;
                 break;
-            case '-':
+            case "-":
                 result = num1-num2;
                 break;
-            case '*':
+            case "*":
                 result = num1*num2;
                 break;
-            case '/':
+            case "/":
                 result = num1/num2;
                 break;
             default:
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(num1, num2, getOperation());//рекурсия
+                System.out.println("Операция не распознана.");
+                result = 22;
         }
         return result;
     }
+    
+
+////////////////////////////////////////////////////////////////// 
+    
 }
